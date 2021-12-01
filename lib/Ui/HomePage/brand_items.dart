6835c9121c548fb1eb558e8ec52118a10d8d4066
ashraf459810 <78394984+ashraf459810@users.noreply.github.com>
2021-12-01 14:14:11 +0300

    done

--- lib/Ui/HomePage/brand_items.dart
+++ lib/Ui/HomePage/brand_items.dart
@@ -18,6 +18,7 @@ class _BrandItemsState extends State<BrandItems> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
+        shrinkWrap: true,
         physics: ScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
         controller: scrollController,
         children: [
@@ -25,7 +26,7 @@ class _BrandItemsState extends State<BrandItems> {
             height: h(50),
           ),
           Container(
-            height: h(itemcount * 79.2),
+            height: h(itemcount * 73.2),
             child: Padding(
               padding: EdgeInsets.all(w(10)),
               child: GridView.builder(
@@ -35,29 +36,34 @@ class _BrandItemsState extends State<BrandItems> {
                 gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                     maxCrossAxisExtent: 200,
                     childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10),
                 itemCount: itemcount.toInt(),
                 itemBuilder: (context, index) {
                   return GestureDetector(
-                    onTap: () {
-                      print(index);
-                    },
-                    child: container(
-                        width: w(200),
-                        hight: h(100),
-                        color: Colors.white,
-                        borderRadius: 20,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          child: Image.asset(
-                            "assets/images/adidasicon.png",
-                            height: h(133),
-                            width: w(200),
-                            fit: BoxFit.cover,
+                      onTap: () {
+                        print(index);
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: container(
+                          borderRadius: 20,
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20)),
+                              child: Image.asset(
+                                "assets/images/adidasicon.png",
+                                height: h(133),
+                                width: w(200),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
-                        )),
-                  );
+                        ),
+                      ));
                 },
               ),
             ),
